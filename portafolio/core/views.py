44567a07026a9a2c99aac8dc71dabe52c5e3384a
Wilson Ludeña <weludenaj@gmail.com>
2022-12-11 18:08:34 -0500

    Cambios iniciales

---
+++ portafolio/core/views.py
@@ -0,0 +1,26 @@
+from cgitb import html
+
+from django.shortcuts import render, HttpResponse
+
+
+html_cabecera = """
+<h1> Mi portafolio personal</h1>
+<ul> 
+    <li> <a href="/">Inicio </a> </li>
+    <li> <a href="/about-me/">Acerca de </a> </li>
+    <li> <a href="/">Contacto </a> </li>
+</ul>
+"""
+# Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request,'core/about.html')
+
+
+def portafolio(request):
+    return render(request,'portfolio/portfolio.html')
+
+def contacto(request):
+    return render(request,'core/contacto.html')
